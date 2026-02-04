e3989be9d232815b311d9aa92a3bcdb79a81c30e
akdtech1 <anandkumardeva@gmail.com>
2026-02-04 18:10:40 +0530

    new commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
+    resource_group_name  = "CICD-Pipeline"
+    storage_account_name = "terraformcicdpipeline"
+    container_name       = "tfcode"
     key                  = "dev.terraform.tfstate"
   }
 }
